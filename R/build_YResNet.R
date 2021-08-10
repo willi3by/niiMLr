3adb6f73a04cffb2be30858b61b5fdc8486257b5
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2021-08-10 17:37:57 -0400

    Delete comment

--- R/build_YResNet.R
+++ R/build_YResNet.R
@@ -97,9 +97,6 @@ build_YResNet <- function(model_params){
 
     keras::layer_dense(model_params$num_classes, activation = model_params$activation, name = "y_dense2")
 
-  # Operands could not be broadcast together with shapes (512, 512, 512, 1) (4, 4, 8, 3)
-  # mulitplied <- keras::layer_multiply(c(conv10, y_bottom))
-
   uncompiled_model <- keras::keras_model(
     inputs,
     outputs = c(conv10, y_bottom)
